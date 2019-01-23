5af461192abfb6d8637cbe0ff56d604b7019c553
lzh <lizhhn@126.com>
2019-01-23 16:37:38 +0800

    firstly commit webpageCrawl.py

---
+++ webpageCrawl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 16:05:34 2019
+
+@author: 明天怎样
+"""
+import requests
+url = 'http://www.baidu.com/'
+res = requests.get(url)
+html = res.text
+with open('web.html', 'w', encoding='utf-8') as f:
+    f.write(html)
