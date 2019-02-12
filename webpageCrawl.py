fdac08d5111eac999d4f4829c20448a22163c97c
lzh <lizhhn@126.com>
2019-02-12 14:28:24 +0800

    commit ML dir and other file

--- webpageCrawl.py
+++ webpageCrawl.py
@@ -7,6 +7,6 @@ Created on Wed Jan 23 16:05:34 2019
 import requests
 url = 'http://www.baidu.com/'
 res = requests.get(url)
-html = res.text
+html = res.text        # 网页源码
 with open('web.html', 'w', encoding='utf-8') as f:
     f.write(html)
